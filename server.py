cc01b7cd51f60546e7dcea83a99fca7b38856940
charbelmarche33 <cmarche@mail.umw.edu>
2024-07-17 23:10:10 -0400

    Still need contact form to work

--- server.py
+++ server.py
@@ -18,11 +18,13 @@ app.config.update(
     MAIL_USE_TSL=False,
 )
 
+
 @app.route("/", methods=["GET"])
 def home():
     return render_template("index.html")
 
-# Run the app
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
 
+# Run the app
+if __name__ == "__main__":
+    # app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host="0.0.0.0", port=8080, debug=True)
